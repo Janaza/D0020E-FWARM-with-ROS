f71d3fce91a39b3a92fd554da203ffc13a4e9696
Jens Gertzell <jens.grt@gmail.com>
2018-03-17 09:36:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,11 +15,11 @@ cd ~/catkin_ws/
 catkin_make
 source ~/catkin_ws/devel/setup.bash
 sudo apt-get install ros-kinetic-lms1xx
-
-
-
+sudo apt-get install rosbash
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc 
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
 printf "\n\n"
 printf "ROS and packages have been installed. Don't forget to set proper network settings on both embeded pc and remote pc. \nIP/subnet/gateway should all be set as they are on the LMS151 LiDAR."
 printf "You should also export the variables ROS_MASTER_URI='http://EMBEDED_PC_IP:11311' and ROS_IP=EMBEDED_PC_IP"
